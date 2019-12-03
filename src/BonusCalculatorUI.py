8757f41d496bb4790290a413c2804df10a657c56
erik.sattelmair <erik.sattelmair@assentis.com>
2019-12-03 20:01:01 +0100

    added result window and closing capabilities

--- src/BonusCalculatorUI.py
+++ src/BonusCalculatorUI.py
@@ -23,22 +23,48 @@ def convert_salary_to_number(salary):
 
     return salary
 
-def print_result(df):
+def get_result(df):
+    result = []
+
     for index, row in df.iterrows():
-        print('Bonus für Mitarbeiter/in %s %s beträgt %s €' % (row.Vorname, row.Name, calculate_bonus(row['Monatsgehalt September'])))
+        result.append('Bonus für Mitarbeiter/in %s %s beträgt %s €' % (row.Vorname, row.Name, calculate_bonus(row['Monatsgehalt September'])))
+
+    return result
 
 def get_excel():
     import_file_path = filedialog.askopenfilename()
     df = read_excel_file(import_file_path, work_sheet_name)
-    print_result(df[df['Tarif'] == 'T1'])
+    create_result_window(get_result(df[df['Tarif'] == 'T1']))
+
+def create_result_window(results):
+    result_window = tk.Toplevel()
+    result_window.wm_title('Ergebnis')
+    result_window.configure(background='lightsteelblue')
+
+    if len(results) == 0:
+        result_row = tk.Label(result_window, text="Keine Ergebnisse", fg='black', bg='lightsteelblue', font = ('helvetica', 8, 'bold'))
+        result_row.pack(side="top", fill="both", expand=True, padx=10, pady=10)
+    else:
+        for result in results:
+            result_row = tk.Label(result_window, text=result, fg='black', bg='lightsteelblue', font = ('helvetica', 8, 'bold'))
+            result_row.pack(side="top", fill="both", expand=True, padx=5, pady=5)
+
+    button1 = tk.Button(result_window, text='Schließen', command = close_windows, bg = 'green', fg = 'white', font = ('helvetica', 12, 'bold'))
+    button1.pack(side='bottom')
+
+def close_windows():
     root.destroy()
 
+def create_main_window():
+    canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue')
+    canvas1.pack()
+
+    browse_button_excel = tk.Button(text = 'Import Excel File', command = get_excel, bg = 'green', fg = 'white', font = ('helvetica', 12, 'bold'))
+    canvas1.create_window(150, 150, window=browse_button_excel)
+
+    root.mainloop()
+
+# ---------------------------------------------------- Main part ------------------------------------------------------------------------------- #
+
 root = tk.Tk()
-
-canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue')
-canvas1.pack()
-
-browseButton_excel = tk.Button(text = 'Import Excel File', command = get_excel, bg = 'green', fg = 'white', font = ('helvetica', 12, 'bold'))
-canvas1.create_window(150, 150, window=browseButton_excel)
-
-root.mainloop()
+create_main_window()
