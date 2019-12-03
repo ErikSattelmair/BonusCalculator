a01044ea2d293461bf9e59ccd3802c06263c5ee8
erik.sattelmair <erik.sattelmair@assentis.com>
2019-12-03 18:13:30 +0100

    Initial commit

---
+++ src/BonusCalculator.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import os
+
+def get_resource_dir():
+    return os.path.dirname(os.path.abspath(__file__)) + os.path.sep + '..' + os.path.sep + 'resources' + os.path.sep
+
+def read_excel_file(excel_file_path, sheet_name):
+    return pd.read_excel (excel_file_path, sheet_name)
+
+def calculate_bonus(salary):
+    return '{:.2f}'.format(salary * 0.02 + 100).replace('.', ',')
+
+def print_result(df):
+    for index, row in df.iterrows():
+        print('Bonus für Mitarbeiter/in %s %s beträgt %s €' % (row.Vorname, row.Name, calculate_bonus(row['Monatsgehalt September'])))
+
+# ------------------------------------------------------------------------------------------------------------------------------------ #
+
+resource_dir_path = get_resource_dir()
+gehaltsabrechnung_excel_path = resource_dir_path + 'Gehaltabrechnung.xlsx'
+sheet_name = 'Tabelle1'
+
+gehalt_df = read_excel_file(gehaltsabrechnung_excel_path, sheet_name)
+print_result(gehalt_df[gehalt_df['Tarif'] == 'T1'])
